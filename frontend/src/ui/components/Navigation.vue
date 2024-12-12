4083f0eaaa12c7a060ebf377db9bc7b824b5e59f
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-12-12 16:19:05 -0400

    feat(frontend): :sparkles: add action to logout in ProfileMenu

--- frontend/src/ui/components/Navigation.vue
+++ frontend/src/ui/components/Navigation.vue
@@ -1,66 +1,57 @@
 <template>
-    <v-navigation-drawer v-model="isDrawerOpen" >
-        <v-list>
-          <v-list-subheader>Menu</v-list-subheader>
-          <v-list-item prepend-icon="mdi-home"><router-link :to="{name: 'dashboard'}">Dashboard</router-link></v-list-item>
-          <v-list-item prepend-icon="mdi-account"><router-link :to="{name: 'users'}">Usuários</router-link></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-  
-      <v-app-bar flat class="border-b" permanent>
-        <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
-        <v-app-bar-title>{{ appName }}</v-app-bar-title>
-  
-        <template #append>
-          <v-btn icon class="mr-2">
-            <v-badge dot color="info">
-              <v-icon icon="mdi-bell-outline"></v-icon>
-            </v-badge>
-          </v-btn>
-  
-          <v-menu>
-            <template #activator="{ props }">
-              <v-avatar v-bind="props">
-                <v-img
-                  cover
-                  src="https://thumbs.dreamstime.com/z/nerd-portrait-young-cheerful-businessman-smiling-36201399.jpg"></v-img>
-              </v-avatar>
-            </template>
-  
-            <v-card min-width="200px">
-              <v-list :lines="false" density="compact" nav>
-                <v-list-item prepend-icon="mdi-account-outline">
-                  <v-list-item-title>Meu perfil</v-list-item-title>
-                </v-list-item>
-  
-                <v-list-item prepend-icon="mdi-heart-outline">
-                  <v-list-item-title>Favoritos</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-card>
-          </v-menu>
+  <v-navigation-drawer v-model="isDrawerOpen">
+      <v-list>
+        <v-list-subheader>Menu</v-list-subheader>
+        <v-list-item prepend-icon="mdi-home"><router-link :to="{name: 'dashboard'}">Dashboard</router-link></v-list-item>
+        <v-list-item prepend-icon="mdi-account"><router-link :to="{name: 'users'}">Usuários</router-link></v-list-item>
+      </v-list>
+  </v-navigation-drawer>
+  <v-app-bar flat class="border-b" permanent>
+    <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
+    <v-app-bar-title>{{ appName }}</v-app-bar-title>
+
+    <template #append>
+      <v-btn icon class="mr-2">
+        <v-badge dot color="info">
+          <v-icon icon="mdi-bell-outline"></v-icon>
+        </v-badge>
+      </v-btn>
+
+      <v-menu>
+        <template #activator="{ props }">
+          <v-avatar v-bind="props">
+            <v-img
+              cover
+              src="https://thumbs.dreamstime.com/z/nerd-portrait-young-cheerful-businessman-smiling-36201399.jpg"></v-img>
+          </v-avatar>
         </template>
-    </v-app-bar>
+
+        <v-card min-width="200px">
+          <v-list :lines="false" density="compact" nav>
+            <v-list-item prepend-icon="mdi-account-outline">
+              <v-list-item-title>Meu perfil</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item prepend-icon="mdi-logout" @click.prevent="logout">
+              <v-list-item-title>Sair</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </template>
+  </v-app-bar>
 </template>
 <script setup>
 import { ref } from 'vue';
-const appName = import.meta.env.VITE_APP_NAME
-const isDrawerOpen = ref(false)
-const isDialogOpen = ref(false)
-const emailRules = [
-  value => {
-    if (value) {
-      return true
-    }
+import { useAuthStore } from '@/stores/auth';
+import { useRouter } from 'vue-router';
 
-    return 'O email é obrigatório';
-  },
-  value => {
-    if (value.includes('@')) {
-      return true;
-    }
+const router        = useRouter();
+const authStore     = useAuthStore();
+const appName       = import.meta.env.VITE_APP_NAME;
+const isDrawerOpen  = ref(false);
 
-    return 'Email inválido'
-  }
-];
+const logout = () => {
+  authStore.logout().finally(() => router.go());
+}
 </script>
