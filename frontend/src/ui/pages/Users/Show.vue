77492e3f312744ba3bd579ccada5bd8685833f8a
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-12-14 00:05:58 -0400

    refactor(frontend): :art: refactor code style to User show

--- frontend/src/ui/pages/Users/Show.vue
+++ frontend/src/ui/pages/Users/Show.vue
@@ -1,28 +1,21 @@
 <template>
     <v-card-title>
       <v-btn @click="openDialog" variant="tonal" color="primary" size="small" :ripple="false"
-        >Visualizar</v-btn
-      >
+        >Visualizar
+      </v-btn>
   
       <v-dialog v-model="isDialogOpen" width="600px">
         <v-card>
             <v-card-title>Visualizar Informações</v-card-title>
-
             <v-card-text>
-                <p><strong>Nome:</strong> {{ user.name }}</p>
-                <p><strong>Email:</strong> {{ user.email }}</p>
-                <p><strong>CPF:</strong> {{ formatITI(user.iti) }}</p>
-                <p><strong>Status:</strong> {{ getStatus(user.status) }}</p>
-            </v-card-text>
-            <!-- <div class="d-flex justify-space-between">
-              <v-card-title></v-card-title>
-      
-              <AddUser :isDialogOpen="isDialogOpen" />
-            </div> -->
-      
+              <p><strong>Nome:</strong> {{ user.name }}</p>
+              <p><strong>Email:</strong> {{ user.email }}</p>
+              <p><strong>CPF:</strong> {{ formatITI(user.iti) }}</p>
+              <p><strong>Status:</strong> {{ getStatus(user.status) }}</p>
+            </v-card-text>      
             <v-card-actions>
-                <v-spacer />
-                <v-btn variant="text" @click="closeDialog">Cancelar</v-btn>
+              <v-spacer />
+              <v-btn variant="text" @click="closeDialog">Cancelar</v-btn>
             </v-card-actions>
         </v-card>
       </v-dialog>
