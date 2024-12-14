6a8847442ae541d98369f1598537dbda76fcbe96
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-12-14 16:21:12 -0400

    feat(frontend): :sparkles: add profile_photo_path in navbar

--- frontend/src/ui/components/Navigation.vue
+++ frontend/src/ui/components/Navigation.vue
@@ -2,22 +2,17 @@
   <v-navigation-drawer v-model="isDrawerOpen">
     <v-list>
       <v-list-subheader>Menu</v-list-subheader>
-      <v-list-item prepend-icon="mdi-home"
-        ><router-link :to="{ name: 'dashboard' }"
-          >Dashboard</router-link
-        ></v-list-item
-      >
+      <v-list-item prepend-icon="mdi-home">
+        <router-link :to="{ name: 'dashboard' }">Dashboard</router-link>
+      </v-list-item>
       <v-list-item prepend-icon="mdi-account">
-        <router-link :to="{ name: 'users' }">
-          Usuários
-        </router-link>
+        <router-link :to="{ name: 'users' }">Usuários</router-link>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
+
   <v-app-bar flat class="border-b" permanent>
-    <v-app-bar-nav-icon
-      @click="isDrawerOpen = !isDrawerOpen"
-    ></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
     <v-app-bar-title>{{ appName }}</v-app-bar-title>
 
     <template #append>
@@ -32,7 +27,8 @@
           <v-avatar v-bind="props">
             <v-img
               cover
-              src="https://thumbs.dreamstime.com/z/nerd-portrait-young-cheerful-businessman-smiling-36201399.jpg"
+              :src="profile?.profile_photo_path || imageDefault"
+              alt="Imagem de perfil"
             ></v-img>
           </v-avatar>
         </template>
@@ -41,12 +37,9 @@
           <v-list :lines="false" density="compact" nav>
             <v-list-item prepend-icon="mdi-account-outline">
               <v-list-item-title>
-                <router-link :to="{ name: 'profile' }">
-                  Meu Perfil
-                </router-link>
+                <router-link :to="{ name: 'profile' }">Meu Perfil</router-link>
               </v-list-item-title>
             </v-list-item>
-
             <v-list-item prepend-icon="mdi-logout" @click.prevent="logout">
               <v-list-item-title>Sair</v-list-item-title>
             </v-list-item>
@@ -56,15 +49,21 @@
     </template>
   </v-app-bar>
 </template>
+
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useAuthStore } from '@/stores/auth'
+import { useMeStore } from '@/stores/me'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 const authStore = useAuthStore()
+const meStore = useMeStore()
 const appName = import.meta.env.VITE_APP_NAME
 const isDrawerOpen = ref(false)
+const imageDefault = 'https://thumbs.dreamstime.com/z/nerd-portrait-young-cheerful-businessman-smiling-36201399.jpg'
+
+const profile = computed(() => meStore.user)
 
 const logout = () => {
   authStore.logout().finally(() => router.go())
