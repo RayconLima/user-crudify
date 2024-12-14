95a76c2b25dfe1b0f822dc707824361b267b97f0
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-12-14 11:03:55 -0400

    feat(backend):

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,19 +4,20 @@ use App\Http\Controllers\Internal\InternalInvitationController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\User\MeController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', LoginController::class)->name('auth.login');
-Route::post('register', [RegisterController::class, 'register'])->name('auth.register');
+Route::post('register', RegisterController::class)->name('auth.register');
 Route::post('invitation/savePassword', [InternalInvitationController::class, 'savePasswordByInvitation'])->name('auth.invitation');
-
-// TODO: remove or use this route
-// Route::get('verify/{token}', [RegisterController::class, 'verify'])->name('verify');
+Route::post('verify-email', VerifyEmailController::class);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/logout', LogoutController::class)->name('auth.logout');
     Route::get('/me', MeController::class)->name('auth.me');
+    Route::post('/me/password', PasswordController::class)->name('auth.password');
+    Route::post('/logout', LogoutController::class)->name('auth.logout');
     Route::apiResource('users', UserController::class);
 });
