7b53437e75e15e306d93f9e39f8caa96c46f16f2
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-12-15 19:52:06 -0400

    fix(backend): :bug: update Pest.php to execute Feature tests

--- backend/tests/Pest.php
+++ backend/tests/Pest.php
@@ -11,8 +11,9 @@
 |
 */
 
-pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+use Tests\TestCase;
+
+uses(TestCase::class)
     ->in('Feature');
 
 /*
