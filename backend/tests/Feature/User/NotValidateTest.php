6b4956dba82b6dfa69f085613cb8dec2bb5e6ce7
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-12-15 19:24:41 -0400

    fix(backend): :fire: remove one test unused

--- backend/tests/Feature/User/NotValidateTest.php
+++ backend/tests/Feature/User/NotValidateTest.php
@@ -16,31 +16,16 @@ describe('validations', function () {
             ->assertJsonValidationErrors([
                 'name' => trans('validation.required', ['attribute' => 'name']),
                 'email' => trans('validation.required', ['attribute' => 'email']),
-                'password' => trans('validation.required', ['attribute' => 'password']),
             ]);
     });
     it('should validate data to return 422 when updating a user', function () {
-        $this->putJson(route('users.update', ['user' => $this->user->id]), [], [
+        $data = [
+            'name'      => '',
+        ];
+        $this->putJson(route('users.update', ['user' => $this->user->id]), $data, [
             'Authorization' => "Bearer {$this->token}"
         ])->assertStatus(422)->assertJsonValidationErrors([
             'name' => trans('validation.required', ['attribute' => 'name']),
         ]);
-    });
-    it('should validate the data to return 422 when updating a user whose password is at least 6 characters long', function () {
-        $data = [
-            'name'      => 'John Smith',
-            'password'  => Str::random(5),
-        ];
-
-        $response = $this->putJson(route('users.update', ['user' => $this->user->id]), $data, [
-            'Authorization' => "Bearer {$this->token}"
-        ]);
-
-        $response->assertStatus(422)
-            ->assertJsonValidationErrors([
-                'password' => trans(
-                    'validation.min.string', [ 'attribute' => 'password', 'min' => 6]
-                ),
-            ]);
     });
 });
