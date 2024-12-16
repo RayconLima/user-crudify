7a4eebed67f76c989abb44c2512f4a686fb740ab
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-12-15 23:41:34 -0400

    feat(frontend): :sparkles: add notfound page

---
+++ frontend/src/ui/errors/Error404.vue
@@ -0,0 +1,34 @@
+<!-- src/components/NotFound.vue -->
+<template>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col class="text-center">
+          <!-- <v-img
+            src="@/assets/404.png"
+            max-width="400"
+            contain
+          ></v-img> -->
+          <h1 class="display-1">404</h1>
+          <h2>Página Não Encontrada</h2>
+          <p>Desculpe, a página que você está procurando não existe.</p>
+          <v-btn color="primary">
+            <RouterLink :to="{ name: 'dashboard' }" class="text-decoration-none text-white">
+              Voltar para o Dashboard
+            </RouterLink>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NotFound',
+  }
+  </script>
+  
+  <style scoped>
+  .fill-height {
+    height: 100vh;
+  }
+  </style>
